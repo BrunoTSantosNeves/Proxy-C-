9cdb385ba36c17d3dea1f74f15e0ca74e1cad7af
bidbibb <tonetti583@gmail.com>
2025-01-30 18:22:29 -0300

    Arquivo AuthController.cs feito

--- ProxyFallbackAPI/Controllers/Security/AuthController.cs
+++ ProxyFallbackAPI/Controllers/Security/AuthController.cs
@@ -13,11 +13,10 @@ namespace ProxyFallbackAPI.Controllers.Security
     {
         private readonly ITokenService _tokenService;
         private readonly IUserService _userService;
-
         public AuthController(ITokenService tokenService, IUserService userService)
         {
-            _tokenService = _tokenService ?? throw new ArgumentNullException(nameof(tokenService));
-            _userService = _userService ?? throw new ArgumentNullException(nameof(userService));
+            _tokenService = tokenService ?? throw new ArgumentNullException(nameof(tokenService));
+            _userService = userService ?? throw new ArgumentNullException(nameof(userService));
         }
 
         /// <summary>
@@ -29,5 +28,23 @@ namespace ProxyFallbackAPI.Controllers.Security
         
         [AllowAnonymous]
         [HttpPost("login")]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                return BadRequest("Nome de usuário ou senha não podem estar vazios.");
+            }
+
+            var user = await _userService.ValidateUserAsync(username, password);
+            if (user == null)
+            {
+                return Unauthorized("Usuário ou senha inválidos.");
+            }
+
+            // Gerar token JWT
+            var token = _tokenService.GenerateToken(user.Username);
+
+            return Ok(new { Token = token });
+        }
     }
 }
